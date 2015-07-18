4450f1db9ec10e5cc3c0f73767366d2b89de795c
Nicholas Wiles <nwiles@airware.com>
2015-07-18 16:54:24 -0700

    fix event log time

--- src/app/js/events/EventLog.js
+++ src/app/js/events/EventLog.js
@@ -7,7 +7,7 @@ var EventItem = Marionette.ItemView.extend({
   template: require('../../tmpl/eventitem.hbs'),
 
   templateHelpers: function() {
-    var time = moment(this.model.get('time'));
+    var time = moment(this.model.get('time')*1000);
     return {
       eventType: util.camelCaseToRegularForm(this.model.get('type')),
       dataJSON: JSON.stringify(this.model.get('data'), null, 4),
